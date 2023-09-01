951817ac15b15bcb5ba7fa4b3abc862f493cfb36
Joe K. Kim <joe.kim@intel.com>
2023-08-31 22:23:41 -0700

    Add init.sh script

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -z "$SHARED_DIR" ]]; then
+    echo "SHARED_DIR envinronment variable must be defined"
+    exit 1
+fi
+
+cd "$SHARED_DIR"
+
+if [[ ! -d "vulcan-git" ]]; then
+    git clone https://github.com/jkk-intel/vulcan.git vulcan-git
+fi
