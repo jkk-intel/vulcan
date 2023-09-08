170ed8a963eb16aee01c5ceae2be952225d32c03
Joe K. Kim <joe.kim@intel.com>
2023-09-08 13:54:33 -0700

    update

--- lib/test.sh
+++ lib/test.sh
@@ -2,13 +2,6 @@
 set -e
 source "$SHARED_DIR/bashlib.sh"
 
-CONST_DOCKER_HTTP_PROXY="http://proxy-chain.intel.com:911"
-CONST_DOCKER_HTTPS_PROXY="http://proxy-chain.intel.com:911"
-CONST_DOCKER_NO_PROXY="localhost,192.168.0.0/16,127.0.0.0/8,::1,intel.com"
-export HTTP_PROXY="$CONST_DOCKER_HTTP_PROXY"
-export HTTPS_PROXY="$CONST_DOCKER_HTTPS_PROXY"
-export NO_PROXY="$CONST_DOCKER_NO_PROXY"
-
 bash "$(__dir)/shell/toolchain/venv/python.sh" \
     -v 3.8 -f "$(__dir)/shell/toolchain/venv/requirements.txt"
 
