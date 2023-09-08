49aaf9b0e9567115e7ada10d3dde378419752a64
Joe K. Kim <joe.kim@intel.com>
2023-09-08 11:35:05 -0700

    update

--- lib/test.sh
+++ lib/test.sh
@@ -10,7 +10,7 @@ export HTTPS_PROXY="$CONST_DOCKER_HTTPS_PROXY"
 export NO_PROXY="$CONST_DOCKER_NO_PROXY"
 
 bash "$(__dir)/shell/toolchain/venv/python.sh" \
-    -v 3.8 -f "$(__dir)/shell/toolchain/venv/requirements.txt"
+    -v 3.29 -f "$(__dir)/shell/toolchain/venv/requirements.txt"
 
 bash "$(__dir)/shell/toolchain/venv/node.sh" \
     -f "$(__dir)/shell/toolchain/venv/package.json"
