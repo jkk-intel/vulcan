38ea87643af984618bde4ea73e36dd8fa606b5d9
Joe K. Kim <joe.kim@intel.com>
2023-09-07 19:27:06 -0700

    update

--- init.sh
+++ init.sh
@@ -8,6 +8,13 @@ fi
 cd "$SHARED_DIR"
 
 {
+    if [[ ! -f 'bashlib.sh' ]]; then
+        curl -sL https://raw.github.com/jkk-intel/bashlib/main/bashlib.sh > bashlib.sh
+        chmod +x bashlib.sh
+    fi
+} >>"$INSTALL_DIR/bashlib_prep.log" 2>&1
+
+{
     if [[ -n "$GITHUB_WORKSPACE" ]]; then
         rm -rf "$GITHUB_WORKSPACE/"*
     fi
@@ -22,10 +29,3 @@ cd "$SHARED_DIR"
     git pull
 
 } >>"$INSTALL_DIR/workflowlib.log" 2>&1
-
-{
-    if [[ ! -f 'bashlib.sh' ]]; then
-        curl -sL https://raw.github.com/jkk-intel/bashlib/main/bashlib.sh > bashlib.sh
-        chmod +x bashlib.sh
-    fi
-} >>"$INSTALL_DIR/bashlib_prep.log" 2>&1
