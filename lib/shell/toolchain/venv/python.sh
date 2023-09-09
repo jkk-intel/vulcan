d4c9d48d36d34149b18edc6ea75f049d833ea73b
Joe K. Kim <joe.kim@intel.com>
2023-09-09 02:11:21 -0700

    update

--- lib/shell/toolchain/venv/python.sh
+++ lib/shell/toolchain/venv/python.sh
@@ -50,6 +50,7 @@ PYTHON_INSTALL_LOCKNAME="pyenv-python-install-$PYTHON_VERSION"
     RESULT=$(trylock "$PYTHON_INSTALL_LOCKNAME" 600 "$VENV_FOLDER/last_used")
     if [[ "$RESULT" == 'should_handle' ]]; then
         try (
+            ff
             mkdir -p "$SHARED_DIR/tmp"
             cat 'dd'
             local INSTALL_LOG="$SHARED_DIR/tmp/install.$PYTHON_VERSION.$REQUIREMENTS_FILE_SHASUM.log"
