1da198d837ebcd29c3fc55fa3b4a724387f7f2a8
Joe K. Kim <joe.kim@intel.com>
2023-09-12 15:57:41 -0700

    update

--- lib/test.sh
+++ lib/test.sh
@@ -3,7 +3,9 @@ source "$SHARED_DIR/bashlib.sh"
 
 # setup_python -v 3.11 -f "shell/toolchain/venv/requirements.txt"
 (
-    cd "$(__dir)/node" && setup_node && use_nvm
+    cd "$(__dir)/node"
+    export NODE_PATH="$(setup_node)"
+    use_nvm
     npm run build
 );
 
