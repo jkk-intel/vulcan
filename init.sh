315fb1a2ac5080241d8e73550d93d4062f8a9e9d
Joe Kim <joe.kim@intel.com>
2024-01-06 00:14:38 -0800

    Add alias for use_builder

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -z "$SHARED_DIR" ]]; then
+if [ -z "$SHARED_DIR" ]; then
     echo "SHARED_DIR envinronment variable must be defined"
     exit 1
 fi
@@ -8,7 +8,7 @@ fi
 cd "$SHARED_DIR"
 
 {
-    if [[ ! -f 'bashlib.sh' ]]; then
+    if [ ! -f 'bashlib.sh' ]; then
         curl -sL https://raw.github.com/jkk-intel/bashlib/main/bashlib.sh > bashlib.sh
         chmod +x bashlib.sh
     fi
@@ -16,7 +16,7 @@ cd "$SHARED_DIR"
 
 {
     
-    if [[ ! -d "cicd" ]]; then
+    if [ ! -d "cicd" ]; then
         git clone https://github.com/jkk-intel/vulcan.git cicd
     fi
     
@@ -29,7 +29,7 @@ cd "$SHARED_DIR"
 
 } >>"$INSTALL_DIR/workflowlib.log" 2>&1
 
-if [[ -n "$GITHUB_WORKSPACE" ]]; then
+if [ -n "$GITHUB_WORKSPACE" ]; then
     shopt -s dotglob
     sudo chown -R "$USER" "$GITHUB_WORKSPACE/"*
     sudo rm -rf "$GITHUB_WORKSPACE/"*
@@ -45,12 +45,13 @@ fi
 
 bash $SHARED_DIR/cicd/lib/shell/toolchain/github-cli/gh.sh
 
+BUILDER_CLI_PACKAGE="intel-build"
 REQUIRED_BUILDER_VERSION='0.0.30'
 {
     use_nvm
     if [ ! command -v builder ] || \
        [ "$(builder --version)" != "$REQUIRED_BUILDER_VERSION" ]; then
-        npm i -g intel-build@$REQUIRED_BUILDER_VERSION
+        npm i -g $BUILDER_CLI_PACKAGE@$REQUIRED_BUILDER_VERSION
     fi
 } >/dev/null 2>&1
 echo "BUILDER_VERSION=$REQUIRED_BUILDER_VERSION"
